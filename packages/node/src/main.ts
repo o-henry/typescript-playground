d3f8207c76d6e5b1562351ab0187de451558cc9e
henry <cksgod16@gmail.com>
2022-03-08 20:39:20 +0900

    add: faker

--- packages/node/src/main.ts
+++ packages/node/src/main.ts
@@ -2,12 +2,18 @@ import express from "express";
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`🚀 server listening on port ${port}`);
+app.post("/", (req, res) => {
+  console.log(req.body);
+  // save data to db
+  res.send("Hi");
+});
+
+app.listen(8000, () => {
+  console.log("server is running on port 8000");
 });
