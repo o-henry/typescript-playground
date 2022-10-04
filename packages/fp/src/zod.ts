8b7f6bf73064dfd5d9628da4bfcedec28a7a0b74
henry <cksgod16@gmail.com>
2022-10-04 11:17:01 +0900

    update

--- packages/fp/src/zod.ts
+++ packages/fp/src/zod.ts
@@ -1,19 +1,20 @@
 import { z } from "zod"
 
-const CalculateRiskProfile = (age: Age): Risk => (age < 60 ? Risk.Low : Risk.Medium)
-
-const _Age = z.number().refine((v: number) => 0 <= v && v < 120, { message: "Age can't be more than 120." })
-const __Age = z.number().refine((age: number) => 0 <= age && age < 120)
-type Age = z.infer<typeof _Age>
-type _Age = z.infer<typeof __Age>
-
 enum Risk {
     Low,
     Medium,
     High,
 }
 
+// fail
+const CalculateRiskProfile = (age: Age): Risk => (age < 60 ? Risk.Low : Risk.Medium)
+const _Age = z.number().refine((v: number) => 0 <= v && v < 120, { message: "Age can't be more than 120." })
+type Age = z.infer<typeof _Age>
+
+// success
 const _CalculateRiskProfile = (age: _Age): Risk => (__Age.parse(age) < 60 ? Risk.Low : Risk.Medium)
+const __Age = z.number().refine((age: number) => 0 <= age && age < 120)
+type _Age = z.infer<typeof __Age>
 
 // console.log(_Age.safeParse(9999))
 // console.log(_Age.parse(90))
